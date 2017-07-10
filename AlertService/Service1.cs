ce9e88180d3de6369565180bc73756495cd573b7
ping.liu <ping.liu@corp.jingle.cn>
2017-07-10 10:58:05 +0800

    init project

---
+++ AlertService/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CSCreateProcessAsUserFromService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MessageBox.Show("A message from AlertService.");
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        // This thread function would launch a child process 
+        // in the interactive session of the logged-on user.
+        public static void MyThreadFunc()
+        {
+            CreateProcessAsUserWrapper.LaunchChildProcess("C:\\Windows\\notepad.exe");
+        }
+    }
+}
